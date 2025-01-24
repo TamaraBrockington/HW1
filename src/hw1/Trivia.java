ff4b86b66a7748557ca6a717625841b75de3b41c
Tamara Brockington <tbrockington@arcadia.edu>
2025-01-24 18:08:46 -0500

    Submission for CS101 HW1

--- src/hw1/Trivia.java
+++ src/hw1/Trivia.java
@@ -7,42 +7,56 @@ public class Trivia {
 		// TODO: Declare an int variable called `lateDays`;
 		// TODO: Set `lateDays` to the correct number based on the syllabus
 
+		int lateDays = 3;
+
 		// TODO: uncomment this line to see if lateDays prints correctly
-		// System.out.println("The number of free late days you get throughout the
-		// semester is " + lateDays);
+		System.out.println("The number of free late days you get throughout the semester is " + lateDays);
 
 		// TODO: Declare a String variable called `woodsEmail`;
 		// TODO: Set `woodsEmail` to Mr. Woods' email address in the syllabus
 
+		String woodsEmail;
+
+		woodsEmail = "woodsj@arcadia.edu";
+
 		// TODO: uncomment this line to see if woodsEmail prints correctly
-		// System.out.println("I can always reach my instructor at " + woodsEmail);
+		System.out.println("I can always reach my instructor at " + woodsEmail);
 
 		double celcius;
 		celcius = 15.0;
 
 		// TODO: Declare a double variable called `fahrenheit`.
+		double fahrenheit;
 		// TODO: Write an expression that calculates the correct fahrenheit temperature
 		// from `celsius`
+
+		fahrenheit = (celcius * 2.0 + 30.0);
+
 		// Note you should NOT calculate the conversion on your own and put the number
 		// in. Make the code do the work.
 
 		// TODO: uncomment this line to see if temperature variables prints correctly
-		// System.out.println(celcius + " degrees celcius is " + fahrenheit + " degrees
-		// fahrenheit.");
+		System.out.println(celcius + " degrees celcius is " + fahrenheit + " degrees fahrenheit.");
 
 		// TODO: declare an int variable called `a` and an int variable called `b`
 		// TODO: set `a` to 100 and set `b` to a multiplied by 3, using an expression
 
+		int a = 100;
+		int b = a * 3;
+
 		// TODO: uncomment the following lines and edit the print statements so that
-		// if a is less than b, "a is smaller" prints out
-		// and if b is less than or equal to a, "b is smaller" prints out
+		assert a < b;
+		System.out.println("a is smaller");
+		assert b < a;
+		System.out.println("b is smaller");
 
 		/*
 		 * if (a < b) { System.out.println(""); } else { System.out.println(""); }
 		 */
 
 		// TODO: Write your own print statement(s) that complete the sentence
-		// "A project I would like to program is " ...
+		System.out.println(
+				"A project I would like to program soon, is to control servo motors. I would also like to learn to code into my computers teermial for problems");
 		// This can be something you want to do in this class or something you know is
 		// a long way down the road but that you would want to make someday
 
